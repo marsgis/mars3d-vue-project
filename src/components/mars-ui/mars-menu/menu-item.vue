a9d0173a99a9d274345ead2ec1d578aa687384da
junjie-mars <17356781611@163.com>
2022-09-20 13:39:55 +0800

    缺少组件

---
+++ src/components/mars-ui/mars-menu/menu-item.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mars-menu-item">
+    <span class="mars-menu-icon" v-if="slots.icon">
+      <slot name="icon"></slot>
+    </span>
+    <span class="mars-menu-text">
+      <slot></slot>
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useSlots } from "vue"
+
+const slots = useSlots()
+</script>
+
+<style scoped lang="less"></style>
---
+++ src/components/mars-ui/mars-menu/sub-menu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mars-sub-menu">
+    <span class="mars-menu-icon" v-if="slots.icon">
+      <slot name="icon"></slot>
+    </span>
+    <span class="mars-menu-text">
+      <slot name="title"></slot>
+    </span>
+    <div class="sub-menu-content" :class="`placement-${props.placement || 'left'}`">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useSlots } from "vue"
+
+const props = defineProps<{
+  placement?: "left" | "middle" | "right"
+}>()
+
+const slots = useSlots()
+</script>
+
+<style scoped lang="less"></style>
